ac8414d1ccd57bc1a145eba2089420f63f497110
Alexis <Alexis@Alexis>
2018-05-16 13:43:08 +0200

    Amélioration plateau ajout javadoc

--- Hearthstone/src/plateau/Plateau.java
+++ Hearthstone/src/plateau/Plateau.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import exception.HearthstoneException;
 import java.lang.Math;
 import joueur.IJoueur;
-
+/**
+ * 
+ * @author Alexis
+ * Cette classe gère le plateau : joueur courant, adversaire, début et fin de partie
+ *
+ */
 public class Plateau implements IPlateau{
 
 	private ArrayList<IJoueur> joueurs = new ArrayList<IJoueur>();
 	private boolean partie = false;
 	private IJoueur courant;
 	private static Plateau plateau = null;
-	
+	/**
+	 * Création du plateau
+	 */
 	public Plateau() {
 		this.courant = null;
 	}
@@ -21,31 +28,49 @@ public class Plateau implements IPlateau{
 			plateau=new Plateau();
 		return plateau;
 	}
-	
+	/**
+	 * Ajoute un joueur à la partie
+	 * @param joueur
+	 * @throws HearthstoneException
+	 * 
+	 */
 	public void ajouterJoueur(IJoueur joueur) throws HearthstoneException {
 		if (joueur == null) throw new HearthstoneException("Joueur vide");
 		// indexage dans arraylist ??
 		if  (joueurs.get(1) != null) throw new HearthstoneException("Dejà deux joueurs");
 		joueurs.add(joueur);
 	}
-
+	/**
+	 * Après vérification du nombre de joueur, démarrage de la partie
+	 */
 	public void demarrerPartie() throws HearthstoneException {
 		if (joueurs.isEmpty()) throw new HearthstoneException("Il n'y a pas de joueur");
 		if  (joueurs.get(1) == null) throw new HearthstoneException("1 seul joueur");
 		courant =joueurs.get((int) Math.random() * 1);
 		partie = true;
 	}
-
+	/**
+	 * Vérifie l'état de la partie
+	 * @return vrai (true) si la partie est démarrée, faux (false) sinon
+	 */
 	public boolean estDemaree() {
 		return partie;
 	}
-
+/**
+ * Termine le tour pour le joueur passé en paramètre
+ * @param joueur
+ * @throws HearthstoneException
+ */
 	public void finTour(IJoueur joueur) throws HearthstoneException {
 		if (partie != true) throw new HearthstoneException("Partie non commencée");
 		if (joueur != courant) throw new HearthstoneException("Joueur non courant");
 		courant = getAdversaire(joueur);
 	}
-
+/**
+ * Terminer la partie, le heros de l'adversaire meurt
+ * @param joueur
+ * @throws HearthstoneException
+ */
 	public void gagnePartie(IJoueur joueur) throws HearthstoneException {
 		// le heros adverse meurt
 		getAdversaire(joueur).getHeros().setVie(0);
@@ -53,7 +78,12 @@ public class Plateau implements IPlateau{
 		partie = false;
 	}
 
-
+/**
+ * Retourne l'adversaire du joueur actuel (passé en paramètre)
+ * @param joueur
+ * @return Adversaire
+ * @throws HearthstoneException
+ */
 	public IJoueur getAdversaire(IJoueur joueur) throws HearthstoneException {
 		if (joueur == null) throw new HearthstoneException("Joueur ne peut être vide");
 		if (!joueurs.contains(joueur)) throw new HearthstoneException("Joueur inconnu");
@@ -66,19 +96,38 @@ public class Plateau implements IPlateau{
 			return joueurs.get(0);
 	}
 
-
+/**
+ * Joueur courant
+ * @return le joueur courant
+ */
 	public IJoueur getJoueurCourant() {
 		if (partie == true)
 			return courant;
 		else
 			return null;
 	}
-
+/**
+ * Met en place le joueur courant ou mise à jour
+ * @param joueur
+ * @throws HearthstoneException
+ */
 	public void setJoueurCourant(IJoueur joueur) throws HearthstoneException {
 		if (partie == false) throw new HearthstoneException("Partie non commencée");
 		if (joueur == null) throw new HearthstoneException("Joueur ne peut être vide");
 		if (!joueurs.contains(joueur)) throw new HearthstoneException("Joueur inconnu");
 		this.courant = joueur;
+	}
+	/**
+	 * Etat du plateau
+	 * @return les infos sur la partie, sur le joueur courant
+	 */
+	public String toString() {
+		if this.estDemaree()
+			partie = "est demarrée";
+			else
+				partie= "n'est pas demarrée";
+		return plateau = "Cette partie " + partie + this.getJoueurCourant() " affronte" + this.getAdversaire(this.getJoueurCourant())"; C'est à vous : "+this.getJoueurCourant().toString();
+		
 	}
 
 }
